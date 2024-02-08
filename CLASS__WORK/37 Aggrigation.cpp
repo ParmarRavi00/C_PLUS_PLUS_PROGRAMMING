0911e48e38236dda94fc9ab2d0a87b4f0934ba8e
Parmar Raviraj Rajendrasinh <parmarraviraj00@gmail.com>
2024-02-08 18:50:37 +0530

    .

--- CLASS__WORK/37 Aggrigation.cpp
+++ CLASS__WORK/37 Aggrigation.cpp
@@ -1,43 +1,57 @@
 #include<iostream>
 using namespace std;
 
-class mark{
+/*
+	Aggregation  provides 'has-a' relationship.
+	
+	e.g Employee 'has-a' address.
+	    
+	    Dhruv 'has-a' Bike.
+*/
+
+class Address {
 	public:
-		float pr;
+		string street,city,state;
 		
-	public:
-		mark(float pr)
+		Address(string street,string city,string state)
 		{
-			this->pr = pr;
+			this->street = street;
+			this->city = city;
+			this->state = state;
 		}
+		
 };
 
-class student{
-	int roll_no;
-	string name;
-	mark *info;
-	
+class Employee {
+	private:
+		Address *address;
 	public:
+		int id;
+		string name;
 		
-		student(int r, string n, mark *i)
+		Employee(int id,string name,Address *address)
 		{
-			roll_no = r;
-			name = n;
-			info = i;
+			this->id = id;
+			this->name = name;
+			this->address = address;
 		}
 		
 		void display()
 		{
-			cout << roll_no << name << info->pr;
-		}
+			cout<<"\nId : "<<id;
+			cout<<"\nName : "<<name;
+			cout<<"\nAddress Line : "<<address->street;
+			cout<<"\nCity : "<<address->city;
+			cout<<"\nState : "<<address->state;
+		}	
+		
 };
+
 
 int main()
 {
-	mark obj(78.50);
-	student obj1(101, "abc", &obj);
-	
-	obj1.display();
-	
+	Address a1("190 CG Road, Opp. GLS","Ahmedabad","Gujarat");
+	Employee e1(101,"Rakesh",&a1);
+	e1.display();
 	return 0;
 }
